480603173a3a9688f1647ac38caf94b952457b57
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2025-05-18 02:02:59 +0600

    Updated A_Chewbaсca_and_Number.cpp

--- A_Chewbaсca_and_Number.cpp
+++ A_Chewbaсca_and_Number.cpp
@@ -1,7 +1,7 @@
 /*
     In the name of Allah, the Most Compassionate and the Most Benevolent.
 */
-// Problem:
+// Problem: https://codeforces.com/problemset/problem/514/A
 // Handle: Mahfujul Tarunno
 #include <bits/stdc++.h>
 using namespace std;
