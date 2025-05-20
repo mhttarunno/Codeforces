41c0be2756197ada204eb0306b7d30991ee98bbc
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2025-05-20 14:56:23 +0600

    Z_Binary_Search.cpp

--- Z_Binary_Search.cpp
+++ Z_Binary_Search.cpp
@@ -5,60 +5,41 @@
 // Handle: Mahfujul Tarunno
 #include <bits/stdc++.h>
 using namespace std;
-#define BoostIO()                     \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(nullptr);                 \
-    cout.tie(nullptr);
-#define all(x) (x).begin(), (x).end()
-#define fli(a, b) for (int i = a; i < b; ++i)
-#define swap(a, b) (a ^= b, b ^= a, a ^= b)
-#define tc     \
-    int tc;    \
-    cin >> tc; \
-    while (tc--)
-#define endl '\n'
 #define int long long
-#define pb push_back
-#define yes cout << "YES" << endl
-#define no cout << "NO" << endl
-#define el cout << endl
-#define txtIO()                       \
-    freopen("input.txt", "r", stdin); \
-    freopen("output.txt", "w", stdout);
-/*-------------------------End of Macros-------------------------*/
-void conquer()
-{
+#define endl "\n"
+
+int32_t main() {
+    ios_base::sync_with_stdio(0),
+    cin.tie(0), 
+    cout.tie(0);
+    
     int a, b;
     cin >> a >> b;
     vector<int> v(a);
-    fli(0, a) cin >> v[i];
-
-    sort(all(v)); // O(n)
-
-    fli(0, b)
-    {
+    for(int i = 0 ; i < a ; ++i) {
         int x;
         cin >> x;
-        // Binary Search Emplementation :
+        v.push_back(x);
+    }
+    sort(v.begin(), v.end());
+    while(b--) {
+        int x;
+        cin >> x;
         int left = 0, right = a - 1;
         bool ok = false;
-        while (left <= right)
-        {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (v[mid] == x)
-            {
+            if (v[mid] == x) {
                 ok = true;
                 break;
-            }
-            else if (v[mid] < x) left = mid + 1;
-            else right = mid - 1;
+            } else if (v[mid] < x)
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
-        if (ok) cout << "found" << endl;
-        else cout << "not found" << endl;
+        if (ok)
+            cout << "found" << endl;
+        else
+            cout << "not found" << endl;
     }
-}
-signed main()
-{
-    BoostIO();
-    conquer();
 }
