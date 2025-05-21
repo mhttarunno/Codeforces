b394685bacfd8132ecf719842d5a3ac2237d5fe1
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2025-05-21 21:53:30 +0600

    A_Dr_TC.cpp

--- A_Dr_TC.cpp
+++ A_Dr_TC.cpp
@@ -5,29 +5,19 @@
 // Handle: Mahfujul Tarunno
 #include <bits/stdc++.h>
 using namespace std;
-#define Implemented ios_base::sync_with_stdio(false);
-#define By cin.tie(0);
-#define Tarunno cout.tie(NULL);
-#define all(a) a.begin(), a.end()
-#define allc(a, x) a.begin(), a.end(), x
-#define fll(x, a, b) for (int x = a; x < b; ++x)
-#define fl(a) for (auto &i : a)
-#define endl '\n'
 #define int long long
-#define pb push_back
-#define yes cout << "YES" << endl
-#define no cout << "NO" << endl
+#define endl "\n"
 
 void conquer() {
     int n;
     string s;
     cin >> n >> s;
     int ans = 0;
-    fll(i, 0, n) {
+    for(int i = 0 ; i < n ; ++i) {
         if(s[i] == '1') ans++;
     }
     int sum = 0;
-    fll(i, 0, n) {
+    for(int i = 0 ; i < n ; ++i) {
         if(s[i] == '1') sum += ans - 1;
         else sum += ans + 1;
     }
@@ -35,11 +25,6 @@ void conquer() {
 }
 
 int32_t main() {
-
-    Implemented By Tarunno
-
-    int tc = 1, Case = 1;
-    cin >> tc;
-    while (tc--)
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     conquer();
 }
