cfc6c24af61fd750668992955175e80364810705
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2025-05-23 00:34:45 +0600

    Z_Binary_Search.cpp

--- Z_Binary_Search.cpp
+++ Z_Binary_Search.cpp
@@ -12,7 +12,7 @@ int32_t main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int a, b;
     cin >> a >> b;
-    vector<int> v(a);
+    vector<int> v;
     for(int i = 0 ; i < a ; ++i) {
         int x;
         cin >> x;
