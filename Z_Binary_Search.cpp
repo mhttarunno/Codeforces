bf96b6c337d33c60392d549903fedd6475896277
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2025-05-23 00:34:01 +0600

    Z_Binary_Search.cpp

--- Z_Binary_Search.cpp
+++ Z_Binary_Search.cpp
@@ -9,10 +9,7 @@ using namespace std;
 #define endl "\n"
 
 int32_t main() {
-    ios_base::sync_with_stdio(0),
-    cin.tie(0), 
-    cout.tie(0);
-    // Binary Search Implementation
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int a, b;
     cin >> a >> b;
     vector<int> v(a);
