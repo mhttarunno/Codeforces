4dc470473b456f4fdf4ef1a5679672ee0e6282c4
Mahfujul Tarunno <90449456+mhttarunno@users.noreply.github.com>
2025-05-24 12:08:33 +0600

    Add files via upload

---
+++ A_Line_Trip.cpp
@@ -0,0 +1,45 @@
+/*
+    In the name of Allah, the Most Compassionate and the Most Benevolent.
+*/
+// Problem: https://codeforces.com/problemset/problem/1901/A
+// Handle: Mahfujul Tarunno
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> v;
+    v.push_back(0);
+    for (int i = 0; i < n; ++i) {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+
+    v.push_back(x);
+    n = v.size();
+
+    int diff = INT_MIN;
+    for (int i = 1; i < n; ++i) {
+        if (i == n - 1)
+            diff = max(diff, 2 * (v[i] - v[i - 1]));
+        else
+            diff = max(diff, (v[i] - v[i - 1]));
+    }
+    cout << diff << endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    solve();
+    return 0;
+}
